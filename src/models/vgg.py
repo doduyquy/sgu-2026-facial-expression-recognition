59adda48d8b6edf5648e4a018371e490033f7575
Luphuc2005 <phucga150625@gmail.com>
2026-04-05 17:19:07 +0700

    Cập nhật Ratio trong Channel và Tăng dropoutblock

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -136,7 +136,7 @@ class SpatialAttention(nn.Module):
         return x * attn
 
 class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, ratio=16):
+    def __init__(self, in_planes, ratio=8):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
